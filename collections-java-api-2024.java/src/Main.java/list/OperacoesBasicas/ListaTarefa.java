a0ab462eda3676f00218d2dcae748df3477e5c75
Amarildo Xavier Marchi <mchmcse@outlook.com>
2024-09-20 18:10:41 -0300

    Collections-java-api-2024

---
+++ collections-java-api-2024.java/src/Main.java/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,13 @@
+package Main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //Atributo
+    private List<Tarefa> TarefaList;    
+
+public ListaTarefa(){
+    this.TarefaList = new ArrayList<>();
+}
+}
